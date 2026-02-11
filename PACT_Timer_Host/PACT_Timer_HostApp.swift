2ba2383d9da83d5ef179c99dd90a40c54b1182b3
Nikolaj Ivancic <nik@congral.us>
2026-02-10 19:59:57 -0800

    Initial Commit

---
+++ PACT_Timer_Host/PACT_Timer_HostApp.swift
@@ -0,0 +1,17 @@
+//
+//  PACT_Timer_HostApp.swift
+//  PACT_Timer_Host
+//
+//  Created by nik on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct PACT_Timer_HostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
